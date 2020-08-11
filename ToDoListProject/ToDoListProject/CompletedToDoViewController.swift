9b891f6e8683cfce3510cc507027aae413b43c78
isabelpt <56853689+isabelpt@users.noreply.github.com>
2020-08-10 21:45:43 -0400

    Iteration 0.75

---
+++ ToDoListProject/ToDoListProject/CompletedToDoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CompletedToDoViewController.swift
+//  ToDoListProject
+//
+//  Created by Isabel Prado-Tucker on 8/10/20.
+//  Copyright © 2020 Isabel Prado-Tucker. All rights reserved.
+//
+
+import UIKit
+
+class CompletedToDoViewController: UIViewController {
+
+    @IBOutlet weak var toDoDisplay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDoListProject/ToDoListProject/ToDoTableViewController.swift
+++ ToDoListProject/ToDoListProject/ToDoTableViewController.swift
@@ -33,7 +33,7 @@ class ToDoTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return listOfToDo.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -49,16 +49,28 @@ class ToDoTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+         // this gives us a single ToDo
+         let eachToDo = listOfToDo[indexPath.row]
+
+         performSegue(withIdentifier: "moveToCompletedToDoVC", sender: eachToDo)
+    }
 
 
-    /*
+
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
+        nextAddToDoVC.previousToDoTVC = self
+        }
     }
-    */
+
+    
 
 }
